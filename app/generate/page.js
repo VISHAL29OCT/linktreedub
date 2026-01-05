9d483a0c016046eb6ac5c75a24365e32931f370b
VISHAL29OCT <77202320+VISHAL29OCT@users.noreply.github.com>
2026-01-05 12:31:21 +0530

    fix generate route and suspense

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,4 +1,6 @@
 "use client"
+export const dynamic = "force-dynamic";
+
 import React, { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import { useSearchParams } from 'next/navigation';
